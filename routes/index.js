6b9439c92213ca5f3a365623c8d5152bad6dc52f
Juan Camilo Morales Niño <jmoralesn@psl.com.co>
2018-12-21 10:26:37 -0600

    Changed title name.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Sistemasa" });
+  res.render("index", { title: "Freelance Juank payments" });
 });
 
 module.exports = router;
